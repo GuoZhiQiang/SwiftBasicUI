3482663ec59921b4b457a74085e4b745fde9a3f2
coding/ios <1029549847@qq.com>
2016-04-11 19:03:41 +0800

    first push

---
+++ SwiftBasicUI/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  SwiftBasicUI
+//
+//  Created by guo on 16/4/7.
+//  Copyright © 2016年 guo. All rights reserved.
+//
+
+import UIKit
+
+//https://developer.apple.com/library/ios/referencelibrary/GettingStarted/DevelopiOSAppsSwift/Lesson3.html#//apple_ref/doc/uid/TP40015214-CH22-SW1
+
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    // MARK:Properties
+    
+    @IBOutlet weak var lb_mealName: UILabel!
+    @IBOutlet weak var txtField_name: UITextField!
+    @IBOutlet var btn_setName: UIButton!
+
+    // MARK:Sys
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        txtField_name.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK:UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    // is called after the text field resigns its first-responder status
+    func textFieldDidEndEditing(textField: UITextField) {
+        lb_mealName.text = textField.text
+    }
+
+    // MARK:Actions
+    
+    @IBAction func btnClick(sender: UIButton) {
+        txtField_name.text = "Default name"
+        
+    }
+
+}
+
